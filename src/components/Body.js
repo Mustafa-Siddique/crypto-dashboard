2306f74c9cc30e7de007bbcdb0b8ed208f5b279c
Mustafa-Siddique <mstfasid@gmail.com>
2023-01-26 22:57:34 +0530

    navbar

---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Body = () => {
+  return (
+    <div className='grid grid-cols-3 gap-4'>
+        <div className="col-span-2"></div>
+        <div className="col-span-1"></div>
+    </div>
+  )
+}
