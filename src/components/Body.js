feb68b77d2857cf7514085ff74b6bd64042bc13c
Mustafa-Siddique <mstfasid@gmail.com>
2023-01-27 12:54:53 +0530

    headlessui/react

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { SearchArea } from './SearchArea'
 
 export const Body = () => {
   return (
-    <div className='grid grid-cols-3 gap-4'>
-        <div className="col-span-2"></div>
-        <div className="col-span-1"></div>
+    <div className="container mx-auto dark:bg-gray-800 dark:text-white mt-12">
+        <div className='grid grid-cols-3 gap-4'>
+        <div className="col-span-2">
+            <SearchArea/>
+        </div>
+        <div className="col-span-1">
+            Nothing
+        </div>
+    </div>
     </div>
   )
 }
